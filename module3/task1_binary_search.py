5eaac7e91077cfc7ead4685fd8ae9fdee487b29e
Andrey Larin <larin.projects@gmail.com>
2017-07-28 17:40:24 +0300

    Added binary search script

---
+++ module3/task1_binary_search.py
@@ -0,0 +1,37 @@
+"""
+В первой строке даны целое число 1≤n≤10^5 и массив A[1…n] из n различных натуральных чисел, не превышающих 10^9, 
+в порядке возрастания, во второй — целое число 1≤k≤10^5 и k натуральных чисел b1,…,bk, не превышающих 10^9. 
+Для каждого i от 1 до k необходимо вывести индекс 1≤j≤n, для которого A[j]=bi, или −1, если 
+такого j нет.
+
+
+Sample Input:
+5 1 5 8 12 13
+5 8 1 23 1 11
+
+Sample Output:
+3 1 -1 1 -1
+"""
+
+
+def binary_search(x, lst):
+    l = 0
+    r = len(lst)
+    while r - l > 1:
+        idx = (l + r) // 2
+        if x < lst[idx]:
+            r = idx
+        else:
+            l = idx
+    return l + 1 if r > 0 and lst[l] == x else -1
+
+
+def main():
+    _, *haystack = [int(i) for i in input().split()]
+    _, *needles = [int(i) for i in input().split()]
+    for needle in needles:
+        print(binary_search(needle, haystack), end=' ')
+
+
+if __name__ == '__main__':
+    main()
---
+++ module3/tests/task1_binary_search-test.py
@@ -0,0 +1,34 @@
+import unittest
+from module3.task1_binary_search import binary_search
+
+
+class TestBinarySearch(unittest.TestCase):
+    def test1(self):
+        haystack = [1, 2, 3, 4, 5, 6]
+        needles = [1, 6]
+        expected = [1, 6]
+        actual = []
+        for needle in needles:
+            actual.append(binary_search(needle, haystack))
+        self.assertEquals(actual, expected)
+
+    def test2(self):
+        haystack = [1, 5, 8, 12, 13, 18, 20, 24, 30]
+        needles = [8, 18, 12]
+        expected = [3, 6, 4]
+        actual = []
+        for needle in needles:
+            actual.append(binary_search(needle, haystack))
+        self.assertEquals(actual, expected)
+
+    def test3(self):
+        haystack = [1, 5, 8, 12, 13, 18, 20, 24, 30, 45, 100, 150, 1000, 2000]
+        needles = [8, 17, 45, 12, 1, 2, 3, 150, 1000, 24, 100, 150, 12, 2000, 30]
+        expected = [3, -1, 10, 4, 1, -1, -1, 12, 13, 8, 11, 12, 4, 14, 9]
+        actual = []
+        for needle in needles:
+            actual.append(binary_search(needle, haystack))
+        self.assertEquals(actual, expected)
+
+    def test4(self):
+        self.assertEqual(-1, binary_search(8, []))
